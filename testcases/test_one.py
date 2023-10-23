457e9527aeaf965d60aa34c5a89ad8795f8ef927
admin <fraulen.shen@gmail.com>
2023-10-23 18:32:14 +0800

    运行参数m k ,注册配置ini文件

--- testcases/test_one.py
+++ testcases/test_one.py
@@ -1,11 +1,16 @@
 # pytest_demo
+import pytest
+
+
+@pytest.mark.p0
 def test_one():
     expect = 1
     actual = 1
     assert expect == actual
 
+
+@pytest.mark.test
 def test_two():
     expect = 1
     actual = 2
     assert expect == actual
-
