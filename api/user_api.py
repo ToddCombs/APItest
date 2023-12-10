b1bf75ce9aad441ae96276367958f3549beb09d4
fraulen.shen@gmail.com <fraulen.shen@gmail.com>
2023-12-10 23:10:16 +0800

    代码优化

--- api/user_api.py
+++ api/user_api.py
@@ -5,22 +5,22 @@ from utils.response_util import process_response
 
 
 def send_code(json_data):
-    '''
+    """
     获取短信验证码
     :param json_data:
     :return:
-    '''
+    """
     response = api_util.get_code(json=json_data)
     # 引入response处理工具并返回
     return process_response(response)
 
 def register(code, mobile):
-    '''
+    """
     注册接口处理数据
     :param code:
     :param mobile:
     :return:
-    '''
+    """
     json_data = {
         "code": str(code),
         "password": "123456",
@@ -30,12 +30,12 @@ def register(code, mobile):
     return process_response(response)
 
 def login(username, password):
-    '''
+    """
     登录接口数据处理
     :param username:
     :param password:
     :return:
-    '''
+    """
     json_data = {
         "username": username, "password": password
     }
@@ -43,11 +43,11 @@ def login(username, password):
     return process_response(response)
 
 def add_shopping_cart(parmas, token):
-    '''
+    """
     添加购物车
     :param parmas:
     :return:
-    '''
+    """
     headers = {
         "Authorization": "JWT " + token
     }
@@ -55,14 +55,36 @@ def add_shopping_cart(parmas, token):
     return process_response(response)
 
 def add_message(data, files, token):
-    '''
+    """
     添加留言
     :param params:
     :param token:
     :return:
-    '''
+    """
     headers = {
         "Authorization": "JWT " + token
     }
+    response = api_util.add_message(data=data, files=files, headers=headers)
+    return process_response(response)
+
+
+def add_shopping_cart2(parmas, headers):
+    """
+    添加购物车
+    :param headers:
+    :param parmas:
+    :return:
+    """
+    response = api_util.shopping_add(json=parmas, headers=headers)
+    return process_response(response)
+
+def add_message2(data, files, headers):
+    """
+    添加留言
+    :param data:
+    :param files:
+    :param headers:
+    :return:
+    """
     response = api_util.add_message(data=data, files=files, headers=headers)
     return process_response(response)
--- testcases/conftest.py
+++ testcases/conftest.py
@@ -6,19 +6,19 @@ from api.user_api import login
 from utils.read import base_data
 
 def get_data():
-    '''
+    """
     简化获取数据函数
     :return:
-    '''
+    """
     return base_data.read_data()
 
 
 @pytest.fixture
 def login_fixture():
-    '''
+    """
     简化登录接口
     :return:
-    '''
+    """
     if 'token' not in os.environ:
         data = get_data()['login_fixture']
         mobile = data['mobile']
@@ -31,4 +31,17 @@ def login_fixture():
         return os.environ['token'], os.environ['mobile']
 
 
-
+@pytest.fixture(scope="session")
+def login_token():
+    """
+    简化登录接口
+    :return:
+    """
+    data = get_data()['login_fixture']
+    mobile = data['mobile']
+    password = data['password']
+    result = login(mobile, password)
+    headers = {
+        "Authorization": "JWT " + result.body["token"]
+    }
+    return headers, mobile
