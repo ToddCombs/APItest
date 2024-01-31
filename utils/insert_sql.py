76a5b8dd29661a2e0b32ab3f3289b9de16a94bd8
admin <fraulen.shen@gmail.com>
2024-01-31 14:17:00 +0800

    一些实际应用小工具

--- utils/insert_sql.py
+++ utils/insert_sql.py
@@ -17,7 +17,8 @@ columns = ['order_code', 'first_channel_code', 'second_channel_code', 'user_id',
            'source', 'delete_flag', 'remark']
 
 for i in range(1, 10001):
-    values = f"('order00aaaa{i}', 'CJ_YT_AHYT', 'gx_ywx_tyb', '7','23555438', '13718415257','1000', '29.90','29.90', '0.00','3000','2','107203477297799','3{i}','0','test')"
+    values = f"('order00aaaa{i}', 'CJ_YT_AHYT', 'gx_ywx_tyb', '7','23555438', '13718415257','1000', '29.90','29.90', " \
+             f"'0.00','3000','2','107203477297799','3{i}','0','test')"
     insert_sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES {values};"
     cursor.execute(insert_sql)
 
